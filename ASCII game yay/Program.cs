642da2692f40b11ddd562a32f20fb0f49597aeac
Oscar Andrade <ailezx@gmail.com>
2019-11-26 22:02:51 -0500

    prueba de weas matematicas

--- ASCII game yay/Program.cs
+++ ASCII game yay/Program.cs
@@ -10,6 +10,28 @@ namespace ASCII_game_yay
     {
         static void Main(string[] args)
         {
+            int z = 6;
+            float b = 10.4f;
+            string str = "According to all know laws of aviation,";
+            bool myBool = true;
+
+
+            int result;
+
+            result = 42 + 10;
+            Console.WriteLine("suma: " + result);
+
+            result = 42 - 10;
+            Console.WriteLine("resta: " + result);
+
+            result = 42 * 10;
+            Console.WriteLine("multiplicacion: " + result);
+
+            result = 42 / 10;
+            Console.WriteLine("division: " + result);
+
+            Console.ReadLine();
+
         }
     }
 }
